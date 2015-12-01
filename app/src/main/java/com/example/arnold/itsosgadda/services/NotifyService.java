d615173f103e77b4dd4e870063f3e692d4bd8338
ARNOL'D CHARYYEV <vl80tk@gmail.com>
2015-12-01 14:26:55 +0100

    Commit phase two

---
+++ app/src/main/java/com/example/arnold/itsosgadda/services/NotifyService.java
@@ -0,0 +1,50 @@
+package com.example.arnold.itsosgadda.services;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import com.example.arnold.itsosgadda.ComActivity;
+import com.example.arnold.itsosgadda.R;
+
+import static android.media.RingtoneManager.TYPE_NOTIFICATION;
+
+/**
+ * Created by Arnol'd on 27.11.2015.
+ */
+public class NotifyService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Uri sound = RingtoneManager.getDefaultUri(TYPE_NOTIFICATION);
+        Intent intent = new Intent(getApplicationContext(), ComActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+        Notification notification =
+                new NotificationCompat.Builder(getApplicationContext())
+                        .setTicker(getText(R.string.notification_something_arrived))
+                        .setContentTitle(getText(R.string.notification_communication_arrived))
+                        .setContentText(getText(R.string.notification_check_for_communication))
+                        .setSound(sound)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentIntent(pendingIntent)
+                        .build();
+
+        notificationManager.notify(1, notification);
+    }
+}
